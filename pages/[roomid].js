2469ce71fefe454bc4e5355cf73889aaca43ba9b
Sam202003 <samruddhchavan05@gmail.com>
2023-12-12 22:48:59 +0530

    SetUp Media Stream

--- pages/[roomid].js
+++ pages/[roomid].js
@@ -1,10 +1,22 @@
 import usePeer from "@/hooks/usePeer";
-import { useSocket } from "@/context/socket"
+import { useSocket } from "@/context/socket";
+import useMediaStream from "@/hooks/useMediaStream";
+import Player from "@/component/Player";
+const Room = () => {
+  const socket = useSocket();
+  const { peer, myId } = usePeer();
+  const { stream } = useMediaStream();
 
-
-const Room = ()=>{
-    const socket  = useSocket();
-   const{peer,myId} =  usePeer()
-}
+  return (
+    <div>
+ <Player
+              url={stream}
+              muted
+              playing
+              PlayerId={myId}
+            />
+    </div>
+  );
+};
 
 export default Room;
